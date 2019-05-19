58ae4ae0bfd10b3b55e6daf7270f51c7d0d0f982
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-19 15:23:43 +0100

    zmiany, zmiany

--- src/main/java/com/sda/algorytymy/App.java
+++ src/main/java/com/sda/algorytymy/App.java
@@ -1,13 +1,40 @@
-package com.sda.algorytymy;
+package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.Arrays;
+
+public class App {
+
+    public static int sum(int[] tab) {
+
+
+        if (tab.length == 0) {
+
+            return 0;
+
+        }
+
+
+        int[] tab2 = new int[tab.length - 1];
+
+        for (int i = 0; i < tab.length - 1; i++) {
+
+            tab2[i] = tab[i + 1];
+
+        }
+
+        return tab[0] + sum(tab2);
+
     }
+
+    public static void main(String[] args) {
+
+        int[] tab = {2, 4, 6, 2, 2};
+
+
+        System.out.println(sum(tab));
+
+
+    }
+
 }
